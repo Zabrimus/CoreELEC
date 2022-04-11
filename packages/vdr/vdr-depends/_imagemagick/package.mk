9549dbc6d169bd02f85c697c6dce8b10f2e1d08e
Zabrimus <github@hannebauer.org>
2022-04-11 19:01:31 +0200

    downgrade ImageMagick, added vdr-plugin-scraper2vdr

--- packages/vdr/vdr-depends/_imagemagick/package.mk
+++ packages/vdr/vdr-depends/_imagemagick/package.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_imagemagick"
-PKG_VERSION="7.1.0-28"
-PKG_SHA256="771538be92d6b1314345d9deb2533bd187ff8701f0f129c25c9c634f9b91f8dc"
+PKG_VERSION="6.9.11-60"
+PKG_SHA256="d32a11dc92ae03b4c85314dec51d68c7a69da49868391f50182d22602b334e1b"
 PKG_LICENSE="ImageMagick License"
 PKG_SITE="https://imagemagick.org"
-PKG_URL="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/ImageMagick/ImageMagick6/archive/refs/tags/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Use ImageMagick to create, edit, compose, or convert digital images."
 
@@ -20,3 +20,4 @@ pre_configure_target() {
 
   export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}${VDR_PREFIX}/lib"
 }
+
