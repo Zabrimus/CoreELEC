2d92f2b1103534ca189f886a42b2c01b6eabd808
Zabrimus <github@hannebauer.org>
2022-04-20 19:03:20 +0200

    systemd units and scripts to switch between Kodi and VDR

--- packages/vdr/_vdr/bin/switch_kodi_vdr.sh
+++ packages/vdr/_vdr/bin/switch_kodi_vdr.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-if [ "$1" = "kodi" ]; then
-  systemctl stop vdropt
-  systemctl start kodi_o
-else
-  systemctl stop kodi_o
-  systemctl start vdropt
+# 
+# This script will be started after the file /storage/.cache/.watch_prg has been changed
+#
+
+# Start either Kodi or VDR on request
+. /opt/tmp/switch_kodi_vdr
+
+if [ "${START_PRG}" = "vdr" ]; then
+   systemctl stop kodi
+   systemctl start vdropt
+elif [ "${START_PRG}" = "kodi" ]; then
+   systemctl stop vdropt
+   systemctl start kodi
 fi
