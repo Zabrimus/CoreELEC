f361b89b78dedc5392d28a53f260b5d996484961
Zabrimus <github@hannebauer.org>
2022-04-27 09:41:20 +0200

    new plugins: cdplayer, dvd, mp3

--- packages/vdr/vdr-depends/_libcdio_paranoia/package.mk
+++ packages/vdr/vdr-depends/_libcdio_paranoia/package.mk
@@ -1,28 +1,30 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_libcdio_paranoia"
-PKG_VERSION="493702cfd256d34bc7197c24f00df0625dce47b5"
-PKG_SHA256="7132dda3dc47857c014077f4b8f6906dfd6e14a55f79cf09f414115145d108c1"
+PKG_VERSION="10.2+2.0.1"
+PKG_SHA256="7a4e257c85f3f84129cca55cd097c397364c7a6f79b9701bbc593b13bd59eb95"
 PKG_LICENSE="GPL3"
-PKG_SITE="http://libcddb.sourceforge.net/index.html"
-PKG_URL="https://github.com/rocky/libcdio-paranoia/archive/${PKG_VERSION}.zip"
-PKG_SOURCE_DIR="libcdio-paranoia-${PKG_VERSION}"
+PKG_SITE="https://www.gnu.org/software/libcdio/"
+PKG_URL="https://github.com/rocky/libcdio-paranoia/archive/release-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="libcdio-paranoia-release-10.2-2.0.1"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="TODO"
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="manual"
 
-PKG_CONFIGURE_OPTS_TARGET="--prefix=${VDR_PREFIX} \
-						   --bindir=${VDR_PREFIX}/bin \
-                           --libdir=${VDR_PREFIX}/lib \
-                           --libexecdir=${VDR_PREFIX}/bin \
-                           --sbindir=${VDR_PREFIX}/sbin \
-                           --sysconfdir=${VDR_PREFIX}/etc \
-                           --disable-cpp-progs \
-                           --disable-example-progs \
-                           --with-gnu-ld \
-                           --with-pic \
-                           --host=XXX \
-                           "
+#PKG_CONFIGURE_OPTS_TARGET="--prefix=${VDR_PREFIX} \
+#						   --bindir=${VDR_PREFIX}/bin \
+#                           --libdir=${VDR_PREFIX}/lib \
+#                           --libexecdir=${VDR_PREFIX}/bin \
+#                           --sbindir=${VDR_PREFIX}/sbin \
+#                           --sysconfdir=${VDR_PREFIX}/etc \
+#                           --disable-cpp-progs \
+#                           --disable-example-progs \
+#                           --with-gnu-ld \
+#                           --with-pic \
+#                           CC=${CC} \
+#                           CXX=${CXX} \
+#                           "
+
 pre_configure_target() {
   # test if prefix is set
   if [ "x${VDR_PREFIX}" = "x" ]; then
@@ -32,5 +34,10 @@ pre_configure_target() {
 
   export LDFLAGS="$(echo ${LDFLAGS} | sed -e "s|-Wl,--as-needed||") -L${SYSROOT_PREFIX}${VDR_PREFIX}/lib"
 
-  ./autogen.sh
+  ./autogen.sh --prefix=${VDR_PREFIX} --disable-cpp-progs --disable-example-progs
+}
+
+configure_target() {
+	# ./configure --prefix=${VDR_PREFIX} --disable-cpp-progs --disable-static --disable-example-progs --host="XXX" --build="XXX"
+	echo
 }
