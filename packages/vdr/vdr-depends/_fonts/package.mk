9dec10b4fdde57fc0d8d0a12172f74f0da6a4d6a
Zabrimus <github@hannebauer.org>
2022-05-08 09:48:53 +0200

    fix font issue

--- packages/vdr/vdr-depends/_fonts/package.mk
+++ packages/vdr/vdr-depends/_fonts/package.mk
@@ -15,6 +15,10 @@ unpack() {
 }
 
 pre_configure_target() {
+  #mkdir -p ${PKG_BUILD}/
+  #cd ${PKG_BUILD}
+  #unzip -f ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.zip
+
   # test if prefix is set
   if [ "x${VDR_PREFIX}" = "x" ]; then
       echo "==> VDR_PREFIX is empty, but must be set"
@@ -25,15 +29,27 @@ pre_configure_target() {
 make_target() {
     FONTDIR=$(echo ${PKG_NAME}-${PKG_VERSION} | sed -e s:_::g)
 
-	mkdir -p ${INSTALL}/storage/.fonts/android
-	cp  ${FONTDIR}/fonts-android-4.3/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/android/
+	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/android
+	cp  ${PKG_BUILD}/${FONTDIR}/fonts-android-4.3/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/android/
 
-	mkdir -p ${INSTALL}/storage/.fonts/sourcesanspro
-	cp  ${FONTDIR}/fonts-sourcesanspro/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/sourcesanspro/
+	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/sourcesanspro
+	cp ${PKG_BUILD}/${FONTDIR}/fonts-sourcesanspro/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/sourcesanspro/
 
-	mkdir -p ${INSTALL}//storage/.fonts/ds-digital
-	cp ${FONTDIR}/fonts-ds-digital/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/ds-digital/
+	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/ds-digital
+	cp ${PKG_BUILD}/${FONTDIR}/fonts-ds-digital/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/ds-digital/
 
-	mkdir -p ${INSTALL}/storage/.fonts/teletext
-    cp ${FONTDIR}/fonts-teletext/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/teletext/
+	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/teletext
+    cp ${PKG_BUILD}/${FONTDIR}/fonts-teletext/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/teletext/
+
+#	mkdir -p ${INSTALL}/storage/.fonts/android
+#	cp  ${FONTDIR}/fonts-android-4.3/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/android/
+
+#	mkdir -p ${INSTALL}/storage/.fonts/sourcesanspro
+#	cp  ${FONTDIR}/fonts-sourcesanspro/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/sourcesanspro/
+
+#	mkdir -p ${INSTALL}/storage/.fonts/ds-digital
+#	cp ${FONTDIR}/fonts-ds-digital/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/ds-digital/
+
+#	mkdir -p ${INSTALL}/storage/.fonts/teletext
+#    cp ${FONTDIR}/fonts-teletext/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/teletext/
 }
