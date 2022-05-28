9ce10076ffbcb860a56111c3f018abc820c5985d
Zabrimus <github@hannebauer.org>
2022-05-28 11:36:55 +0200

    add font vdrsymbols and font directory /storage/.config/vdropt/fonts

--- packages/vdr/vdr-depends/_fonts/package.mk
+++ packages/vdr/vdr-depends/_fonts/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="_fonts"
-PKG_VERSION="e37c7631ae3d86e5096bfe6d4fff4ed9c6639850"
-PKG_SHA256="7a9ac2613389ccbe0e8e9a6ac824063f7c96bd859e3b2fdfb3dd70600b6772d7"
+PKG_VERSION="99e52de86451d1e125ffcf33ded7115ad6827fd7"
+PKG_SHA256="1ec0f109e078f712d779edbbcc171d62ccd885c89e7dabdbc6fe446f2257481e"
 PKG_LICENSE="Apache License"
 PKG_SITE="https://github.com/Zabrimus/fonts"
 PKG_URL="https://github.com/Zabrimus/fonts/archive/${PKG_VERSION}.zip"
@@ -15,10 +15,6 @@ unpack() {
 }
 
 pre_configure_target() {
-  #mkdir -p ${PKG_BUILD}/
-  #cd ${PKG_BUILD}
-  #unzip -f ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.zip
-
   # test if prefix is set
   if [ "x${VDR_PREFIX}" = "x" ]; then
       echo "==> VDR_PREFIX is empty, but must be set"
@@ -41,15 +37,6 @@ make_target() {
 	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/teletext
     cp ${PKG_BUILD}/${FONTDIR}/fonts-teletext/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/teletext/
 
-#	mkdir -p ${INSTALL}/storage/.fonts/android
-#	cp  ${FONTDIR}/fonts-android-4.3/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/android/
-
-#	mkdir -p ${INSTALL}/storage/.fonts/sourcesanspro
-#	cp  ${FONTDIR}/fonts-sourcesanspro/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/sourcesanspro/
-
-#	mkdir -p ${INSTALL}/storage/.fonts/ds-digital
-#	cp ${FONTDIR}/fonts-ds-digital/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/ds-digital/
-
-#	mkdir -p ${INSTALL}/storage/.fonts/teletext
-#    cp ${FONTDIR}/fonts-teletext/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/storage/.fonts/teletext/
+	mkdir -p ${INSTALL}/usr/local/vdrshare/fonts/vdrsymbols
+    cp ${PKG_BUILD}/${FONTDIR}/vdrsymbols/{*.ttf,fonts.dir,fonts.scale} ${INSTALL}/usr/local/vdrshare/fonts/vdrsymbols/
 }
