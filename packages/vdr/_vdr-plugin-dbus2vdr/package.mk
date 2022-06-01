1a5ef68f0e589dbbc85fde075b3bc2229fc15908
Zabrimus <github@hannebauer.org>
2022-06-01 15:13:19 +0200

    removed some error message from vdr-dbus-send.sh

--- packages/vdr/_vdr-plugin-dbus2vdr/package.mk
+++ packages/vdr/_vdr-plugin-dbus2vdr/package.mk
@@ -43,6 +43,7 @@ post_makeinstall_target() {
   # move other files
   mkdir -p ${INSTALL}/usr/local/bin
   mv ${INSTALL}/usr/share/vdr-plugin-dbus2vdr/* ${INSTALL}/usr/local/bin
+  cp $(get_pkg_directory _vdr-plugin-dbus2vdr)/config/vdr-dbus-send.sh ${INSTALL}/usr/local/bin
 
   cp $(get_pkg_directory _vdr-plugin-dbus2vdr)/config/de.tvdr.vdr.conf ${INSTALL}/etc/dbus-1/system.d/de.tvdr.vdr.conf
 
