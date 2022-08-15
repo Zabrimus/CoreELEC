aa231ee3f91f418e0878e7d68e371a1a56d63dde
Zabrimus <github@hannebauer.org>
2022-08-15 13:42:26 +0200

    add OS_MAJOR (prettier directory name)

--- build-local.sh
+++ build-local.sh
@@ -154,7 +154,7 @@ create_vdr_image() {
   if [ "${MAKE_COREELEC}" = "1" ]; then
     VDR_PREFIX="/usr/local" BUILD_SUFFIX="${SUFFIX}" make image
   else
-    VDR_PREFIX="/usr/local" BUILD_SUFFIX="${SUFFIX}" DISTRO="LibreELEC" PROJECT=Amlogic DEVICE=AMLGX ARCH=arm make image
+    VDR_PREFIX="/usr/local" BUILD_SUFFIX="${SUFFIX}" DISTRO="LibreELEC" PROJECT=Amlogic DEVICE=AMLGX ARCH=arm OS_MAJOR="11.0" make image
   fi;
 }
 
